0dd325d1f063ebe1d6a226f29361b018e282c116
a1m0ctane <amir.khan.offenbach.main@gmail.com>
2020-01-27 16:30:05 +0000

    UFT commit

--- Booking System/Booking Process/TestEntities.cs
+++ Booking System/Booking Process/TestEntities.cs
@@ -18,23 +18,10 @@ using System;
     	throw new Exception(("A data source with the specified name does not exist."));
     return dataSourceNameToDataSource[dataSourceName];
     }
-    public HP.ST.Ext.BasicActivities.DataFetchActivity DataFetchActivity23 = null;
     public HP.ST.Ext.BasicActivities.StartActivity StartActivity1 = null;
     public HP.ST.Fwk.RunTimeFWK.CompositeActivities.Loop<Loop2Input> Loop2 = null;
-    public HP.ST.Ext.BasicActivities.DataExporterActivity DataExporterActivity25 = null;
-    public HP.ST.Ext.BasicActivities.DataExporterCloseActivity DataExporterCloseActivity26 = null;
     public HP.ST.Ext.BasicActivities.EndActivity EndActivity3 = null;
-    public HP.ST.Ext.BasicActivities.DataDisconnectActivity DataDisconnectActivity24 = null;
     public HP.ST.Fwk.RunTimeFWK.CompositeActivities.Sequence Sequence22 = null;
-    public HP.ST.Ext.UFTActivity.QtpTestActivity CallQTPTest13 = null;
-    public HP.ST.Fwk.RunTimeFWK.CompositeActivities.IfElse<IfElse14Input> IfElse14 = null;
-    public HP.ST.Fwk.RunTimeFWK.CompositeActivities.IfElseBranch IfElseBranch15 = null;
-    public HP.ST.Fwk.RunTimeFWK.CompositeActivities.IfElseBranch IfElseBranch16 = null;
-    public HP.ST.Ext.UFTActivity.QtpTestActivity CallQTPTest19 = null;
-    public HP.ST.Ext.UFTActivity.QtpTestActivity CallQTPTest18 = null;
-    public HP.ST.Ext.UFTActivity.QtpTestActivity CallQTPTest20 = null;
-    public HP.ST.Ext.UFTActivity.QtpTestActivity CallQTPTest21 = null;
-    public HP.ST.Ext.BasicActivities.ReportMessageActivity ReportMessageActivity17 = null;
     
     }
     
